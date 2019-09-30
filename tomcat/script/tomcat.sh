c8646375532adfeefb981b9c618688db0f0e4202
noah-luo <lg987523@gmail.com>
2019-09-30 18:01:41 +0800

    修改配置我呢间-未完

--- tomcat/script/tomcat.sh
+++ tomcat/script/tomcat.sh
@@ -4,6 +4,7 @@ root_dir=$(dirname $(dirname $script_dir))
 tomcat_dir=$root_dir/tomcat    #tomcat主目录
 app_dir=$root_dir/instances    #多实例主目录
 
+#JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms6144m -Xmx6144m -XX:NewSize=1024m -XX:MaxNewSize=2048m -XX:PermSize=512m -XX:MaxPermSize=512m -XX:MaxTenuringThreshold=10 -XX:NewRatio=2 -XX:+DisableExplicitGC"
 export JAVA_OPTS="-Xms100m -Xmx200m"
 export JAVA_HOME="/usr/java/jdk1.8.0_121"
 export CATALINA_HOME=$tomcat_dir
