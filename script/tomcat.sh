f5ac344dd10bb06fa3af2d383282c800423e4082
noah-luo <41908393+noah-luo@users.noreply.github.com>
2019-10-12 11:15:26 +0800

    Update tomcat.sh
    执行命令前,添加sh命令

--- script/tomcat.sh
+++ script/tomcat.sh
@@ -14,22 +14,22 @@ export CATALINA_BASE=$script_dir
 
 case $1 in
     start)
-        $CATALINA_HOME/bin/catalina.sh start
+        sh $CATALINA_HOME/bin/catalina.sh start
         echo start success!!
         ;;
     stop)
-        $CATALINA_HOME/bin/catalina.sh stop
+        sh $CATALINA_HOME/bin/catalina.sh stop
         echo stop success!!
         ;;
     restart)
-        $CATALINA_HOME/bin/catalina.sh restart
+        sh $CATALINA_HOME/bin/catalina.sh restart
         echo restart success!!
         ;;
     version)
-        $CATALINA_HOME/bin/catalina.sh version
+        sh $CATALINA_HOME/bin/catalina.sh version
         ;;
     configtest)
-        $CATALINA_HOME/bin/catalina.sh configtest
+        sh $CATALINA_HOME/bin/catalina.sh configtest
         ;;
 esac
 exit 0
